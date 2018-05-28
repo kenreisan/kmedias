a4f824c7c56d54f67d372ec335f0333debe1d502
Eduardo Romero <eduardodrh@gmail.com>
2018-05-28 12:06:47 -0500

    version inicial - jugando con numpy

--- kmedias.py
+++ kmedias.py
@@ -1,13 +1,31 @@
 import numpy as np
 
-
 def test():
-	a = np.matrix('1,2;3,5')
+	a = np.matrix([[1],[1]])
+	b = np.matrix('2;1')
+	c = np.matrix('4;3')
+	d = np.matrix('5;4')
 
-	a = a*a
+	d = np.matrix('0,0,0,0')
 
-	print(a)
+	print(d)
 
-	print(a[0,0])
+	q=2
+	w=3
+	r=4
+	t=5
+	d = np.matrix([[q,w,r,t]])
 
-test()
+	print(d)
+
+	d[0,3] = 20
+
+	print(d)
+
+	print(np.size(a))
+
+	for x in d:
+		print(x[0,0])
+
+if __name__ == '__main__':
+	test()
