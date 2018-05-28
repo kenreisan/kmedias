206dd92cb33f4c53627a42bb8a72bcc009c117d7
Eduardo Romero <eduardodrh@gmail.com>
2018-05-28 13:12:26 -0500

    version 0.1 - primera iteracion

--- kmedias.py
+++ kmedias.py
@@ -1,31 +1,19 @@
+import math
 import numpy as np
 
 def test():
-	a = np.matrix([[1],[1]])
-	b = np.matrix('2;1')
-	c = np.matrix('4;3')
-	d = np.matrix('5;4')
-
-	d = np.matrix('0,0,0,0')
-
-	print(d)
-
-	q=2
-	w=3
-	r=4
-	t=5
-	d = np.matrix([[q,w,r,t]])
-
-	print(d)
+	a = np.matrix('1,1;2,1;4,3;5,4')
+	m1 = np.matrix('1,1;2,1')
+	d = np.matrix('0.0,0.0,0.0,0.0;0.0,0.0,0.0,0.0')
 
 	d[0,3] = 20
 
+	for i in range(0,np.shape(d)[0]):
+		for j in range(0,np.shape(d)[1]):
+			d[i,j] = math.sqrt(float(((float(a[j,0]) - float(m1[i,0]))**2 + (float(a[j,1]) - float(m1[i,1]))**2)))
+			#print(float(d[i,j]))
+
 	print(d)
-
-	print(np.size(a))
-
-	for x in d:
-		print(x[0,0])
 
 if __name__ == '__main__':
 	test()
