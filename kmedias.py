3aa383c3f280d1d09d648ac914d6d7f5dedbd7dd
Eduardo D. Romero Hurtado <eduardodrh@gmail.com>
2018-05-29 21:46:27 -0500

    Se quitaron variables innecesarias
    Se limpio algo el codigo, se quitaron variables y numeros magicos.
    El codigo aun requiere optimizacion, y aun sigue funcionando con
    K = 2.

--- kmedias.py
+++ kmedias.py
@@ -1,106 +1,114 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 *-*
 
 """
-Romero Hurtado Eduardo David
-
-VERSION 0.6
-28 MAYO 2018
-
 Programa de agrupamiento K-medias.
-
-El programa esta en una etapa muy preeliminar y solo funciona con k=2,
-es decir que solo hace agrupamientos con 2 medias. 
-Falta optimizarlo y mejorar el codigo que esta muy feo.
 """
 
 import math
 import numpy as np
 
-def kmedia():
+def kmedia(puntos,medias):
 	"""Algoritmo de agrupamiento k-medias.
 
 	Agrupa los puntos dependiendo del centroide mas cercano.
-	Datos:	a = matriz de puntos.
-			m1= matriz de medias (centroides).
-			d = matriz vacia donde se almacenan las distancias de
-				cada pto. a cada centroide.
-			g = lista con los grupos creados.
+	Datos:	puntos = matriz de puntos.
+			medias = matriz de medias (centroides).
 
 	Tanto <m2> como <gn> se utilizan para el recalculo de medias y
 	la nueva lista de grupos de puntos.
 	"""
-
-	a = np.matrix('1,1;2,1;4,3;5,4')
-	m1 = np.matrix('1,1;2,1')
-	d = np.matrix('0.0,0.0,0.0,0.0 ; 0.0,0.0,0.0,0.0')
-
-	#a = np.matrix('8,10;3,10.5;7,13.5;5,18;5,13;6,9;9,11;3,18;8.5,12;8,16')
-	#d = np.matrix('0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 ; 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0')
-	#m1 = np.matrix('8,10;5,13')
 	
-	m2 = np.matrix('0.0,0.0 ; 0.0,0.0')
-	g = [[],[]]
-	gn= [[],[]]
-
+	medias_new = np.zeros((np.shape(medias)[0],np.shape(medias)[1]), dtype=float)
+	gruponew= [[],[]]
 	numero = 1
 
-	print('\nITERACION: ' + str(numero) + '\n')
-	g = distancia(d,a,m1,g)
-	print('puntos pertenecientes a ' + str(m1[0]) + ':\n' + str(g[0]))
-	print('puntos pertenecientes a ' + str(m1[1]) + ':\n' + str(g[1]))
+	print('\n============= ITERACION: ' + str(numero) + ' =============\n')
+	
+	g = distancia(puntos,medias)
 
-	numero += 1
+	while (np.array_equal(g,gruponew) != True):
 
-	while (np.array_equal(g,gn) != True):
-
+		#Esta parte se tiene que arreglar para evitar codigo repetido.
 		for c in g[0]:
-			m2[0,0] = c[0] + m2[0,0]
-			m2[0,1] = c[1] + m2[0,1]
-		m2[0,0] = m2[0,0]/len(g[0])
-		m2[0,1] = m2[0,1]/len(g[0])
+			medias_new[0,0] = c[0] + medias_new[0,0]
+			medias_new[0,1] = c[1] + medias_new[0,1]
+		if (len(g[0]) >= 0):
+			medias_new[0,0] = medias_new[0,0]/len(g[0])
+			medias_new[0,1] = medias_new[0,1]/len(g[0])
+		else:
+			print("Error, division por cero")
+			break
 
 		for c in g[1]:
-			m2[1,0] = c[0] + m2[1,0]
-			m2[1,1] = c[1] + m2[1,1]
-		m2[1,0] = m2[1,0]/len(g[1])
-		m2[1,1] = m2[1,1]/len(g[1])
+			medias_new[1,0] = c[0] + medias_new[1,0]
+			medias_new[1,1] = c[1] + medias_new[1,1]
+		if (len(g[1]) >= 0):
+			medias_new[1,0] = medias_new[1,0]/len(g[1])
+			medias_new[1,1] = medias_new[1,1]/len(g[1])
+		else:
+			print("Error, division por cero")
+			break
 
-		print('\nNueva medias:\n' + str(m2))
+		print('\nNuevas medias:\n' + str(medias_new))
 
-		print('\nITERACION: ' + str(numero) + '\n')
-		gn = distancia(d,a,m2,gn)
-		print('puntos pertenecientes a ' + str(m2[0]) + ':\n' + str(gn[0]))
-		print('puntos pertenecientes a ' + str(m2[1]) + ':\n' + str(gn[1]))
 		numero += 1
+		print('\n============= ITERACION: ' + str(numero) + ' =============\n')
+		gruponew = distancia(puntos,medias_new)
 
-		if (np.array_equal(g,gn) != True):
-			g = gn
-			m2 = np.matrix('.0,.0;.0,.0')
-			gn = [[],[]]
+		if (np.array_equal(g,gruponew) != True):
+			g = gruponew
+			medias_new = np.empty((np.shape(medias)[0],np.shape(medias)[1]))
+			gruponew = [[],[]]
 
-def distancia(d,a,m1,g):
-	"""Calcula la dist. eclidiana de cada pto. a cada centroide.
 
-	Recibe: d = matriz vacia donde se almacenaran las dist.
-			a = matriz de puntos a agrupar.
-			m1= matriz de las medias.
-			g = tupla donde se agrupan los puntos.
+
+def distancia(puntos,medias):
+	"""Calcula la dist. y agrupa puntos a cada centroide.
+
+	Recibe: puntos = matriz de puntos a agrupar.
+			medias = matriz de las medias.
 
 	Devuelve: Lista con los puntos agrupados a cada centroide.
 	"""
+	grupos = []
+	dist = np.empty((np.shape(medias)[0],np.shape(puntos)[0]))
 
-	for i in range(0,np.shape(d)[0]):
-		for j in range(0,np.shape(d)[1]):
-			d[i,j] = math.sqrt((a[j,0] - m1[i,0])**2 + (a[j,1] - m1[i,1])**2)
-	print('Distancias:\n' + str(d) + '\n')
+	for totales in medias:
+		grupos.append([])
 
-	for x in range(0,np.shape(d)[1]):
-		if d[0,x] < d[1,x]:
-			g[0].append([a[x,0],a[x,1]])
+	for i in range(0,np.shape(dist)[0]):
+		for j in range(0,np.shape(dist)[1]):
+			if (np.array_equal(puntos[j],medias[i]) == True):
+				dist[i,j] = 0
+			else:
+				dist[i,j] = math.sqrt((puntos[j,0] - medias[i,0])**2 + (puntos[j,1] - medias[i,1])**2)
+
+	print('Distancias:\n' + str(dist) + '\n')
+
+	for x in range(0,np.shape(dist)[1]):
+		if dist[0,x] < dist[1,x]:
+			grupos[0].append([puntos[x,0],puntos[x,1]])
 		else:
-			g[1].append([a[x,0],a[x,1]])
-	return g
+			grupos[1].append([puntos[x,0],puntos[x,1]])
+
+	print('Puntos pertenecientes a ' + str(medias[0]) + ':\n' + str(grupos[0]) + '\n')
+	print('Puntos pertenecientes a ' + str(medias[1]) + ':\n' + str(grupos[1]))
+	
+	return grupos
 
 
 if __name__ == '__main__':
-	kmedia()
+
+	puntos = np.matrix('1,12.5;3,10.5;3,12.5;3,14.5;3,18;5,18;5,16;5,14.5;5,13;6,9;8,10;9,11;8.5,12;7,13.5;8,16;0.5,10.5')
+	medias = np.matrix('3,14.5;9,11')
+
+	#puntos = np.matrix('1,1;2,1;4,3;5,4')
+	#medias = np.matrix('1,1;2,1')
+
+	#puntos = np.matrix('8,10;3,10.5;7,13.5;5,18;5,13;6,9;9,11;3,18;8.5,12;8,16')
+	#medias = np.matrix('8,10;5,13')
+
+	kmedia(puntos,medias)
+
+	print('\nFIN DEL ALGORITMO.')
